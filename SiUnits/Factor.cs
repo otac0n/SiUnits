d7e0d288c00c35bf95c4f7318391ec97ee434bd4
John Gietzen <john@gietzen.us>
2022-06-28 02:00:10 -0700

    Moved Factor simplification into base class.

--- SiUnits/Factor.cs
+++ SiUnits/Factor.cs
@@ -3,6 +3,8 @@
 namespace SiUnits
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     /// <summary>
     /// The base class for factors in SI Units.
@@ -48,6 +50,90 @@ namespace SiUnits
         {
             var factor = new CompositeFactor(factors);
             return factor.Factors.Count == 1 ? factor.Factors[0] : factor;
+        }
+
+        /// <summary>
+        /// Collapses a collection of factors into the simplest form.
+        /// </summary>
+        /// <param name="factors">The collection of factors to simplify.</param>
+        /// <returns>A simplified list of factors.</returns>
+        public static List<Factor> SimplifyFactors(IEnumerable<Factor> factors)
+        {
+            var singleFactor = new Factor[1];
+            var values = new Dictionary<object, Factor>();
+            foreach (var factor in factors)
+            {
+                IEnumerable<Factor> subFactors;
+                if (factor is CompositeFactor composite)
+                {
+                    subFactors = composite.Factors;
+                }
+                else
+                {
+                    singleFactor[0] = factor;
+                    subFactors = singleFactor;
+                }
+
+                foreach (var subFactor in subFactors)
+                {
+                    switch (subFactor)
+                    {
+                        case NameFactor name:
+                            if (name.Power != 0)
+                            {
+                                if (values.TryGetValue(name.Name, out var value))
+                                {
+                                    var newPower = ((NameFactor)value).Power + name.Power;
+                                    if (newPower == 0)
+                                    {
+                                        values.Remove(name.Name);
+                                    }
+                                    else
+                                    {
+                                        values[name.Name] = new NameFactor(name.Name, newPower);
+                                    }
+                                }
+                                else
+                                {
+                                    values[name.Name] = name;
+                                }
+                            }
+
+                            break;
+
+                        case NumberFactor number:
+                            if (number.Number != 1 && number.Power != 0)
+                            {
+                                if (values.TryGetValue(number.Number, out var value))
+                                {
+                                    var newPower = ((NumberFactor)value).Power + number.Power;
+                                    if (newPower == 0)
+                                    {
+                                        values.Remove(number.Number);
+                                    }
+                                    else
+                                    {
+                                        values[number.Number] = new NumberFactor(number.Number, newPower);
+                                    }
+                                }
+                                else
+                                {
+                                    values[number.Number] = number;
+                                }
+                            }
+
+                            break;
+
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+
+            var result = values.Count == 0
+                ? new List<Factor> { Factors.One }
+                : values.Values.ToList();
+            return result;
         }
 
         /// <summary>
