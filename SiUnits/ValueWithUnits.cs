5c0415e010214dc31e442adb0957ed9170c49cde
John Gietzen <john@gietzen.us>
2024-11-09 15:58:57 -0800

    Implement static math interfaces.

--- SiUnits/ValueWithUnits.cs
+++ SiUnits/ValueWithUnits.cs
@@ -9,7 +9,17 @@ namespace SiUnits
     /// Represents a scalar value with units attached.
     /// </summary>
     /// <typeparam name="T">The type of value with units attached.</typeparam>
-    public readonly struct ValueWithUnits<T> : IEquatable<ValueWithUnits<T>>
+    public readonly struct ValueWithUnits<T> :
+        IEquatable<ValueWithUnits<T>>,
+        IEqualityOperators<ValueWithUnits<T>, ValueWithUnits<T>, bool>,
+        IAdditionOperators<ValueWithUnits<T>, ValueWithUnits<T>, ValueWithUnits<T>>,
+        ISubtractionOperators<ValueWithUnits<T>, ValueWithUnits<T>, ValueWithUnits<T>>,
+        IMultiplyOperators<ValueWithUnits<T>, T, ValueWithUnits<T>>,
+        IDivisionOperators<ValueWithUnits<T>, T, ValueWithUnits<T>>,
+        IMultiplyOperators<ValueWithUnits<T>, Factor<T>, ValueWithUnits<T>>,
+        IDivisionOperators<ValueWithUnits<T>, Factor<T>, T>,
+        IMultiplyOperators<ValueWithUnits<T>, ValueWithUnits<T>, ValueWithUnits<T>>,
+        IDivisionOperators<ValueWithUnits<T>, ValueWithUnits<T>, ValueWithUnits<T>>
         where T : IFloatingPoint<T>, IPowerFunctions<T>
     {
         /// <summary>
@@ -33,27 +43,71 @@ namespace SiUnits
         /// </summary>
         public T Value { get; }
 
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator -(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
             new ValueWithUnits<T>(left.Value - right / left.Units, left.Units);
 
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked -(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
+            new ValueWithUnits<T>(checked(left.Value - right / left.Units), left.Units);
+
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator *(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
             new ValueWithUnits<T>(left.Value * right.Value, left.Units * right.Units);
 
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked *(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
+            checked(new ValueWithUnits<T>(left.Value * right.Value, left.Units * right.Units));
+
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator *(ValueWithUnits<T> left, Factor<T> right) =>
+            new ValueWithUnits<T>(left.Value, left.Units * right);
+
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked *(ValueWithUnits<T> left, Factor<T> right) =>
+            new ValueWithUnits<T>(left.Value, checked(left.Units * right));
+
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator *(ValueWithUnits<T> left, T right) =>
             new ValueWithUnits<T>(left.Value * right, left.Units);
 
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked *(ValueWithUnits<T> left, T right) =>
+            new ValueWithUnits<T>(checked(left.Value * right), left.Units);
+
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator *(T left, ValueWithUnits<T> right) =>
             new ValueWithUnits<T>(left * right.Value, right.Units);
 
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked *(T left, ValueWithUnits<T> right) =>
+            new ValueWithUnits<T>(checked(left * right.Value), right.Units);
+
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator /(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
             new ValueWithUnits<T>(left.Value / right.Value, left.Units / right.Units);
 
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked /(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
+            checked(new ValueWithUnits<T>(left.Value / right.Value, left.Units / right.Units));
+
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator /(ValueWithUnits<T> left, T right) =>
             new ValueWithUnits<T>(left.Value / right, left.Units);
 
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked /(ValueWithUnits<T> left, T right) =>
+            new ValueWithUnits<T>(checked(left.Value / right), left.Units);
+
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator /(T left, ValueWithUnits<T> right) =>
             new ValueWithUnits<T>(left / right.Value, right.Units.Pow(-1));
 
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked /(T left, ValueWithUnits<T> right) =>
+            new ValueWithUnits<T>(checked(left / right.Value), right.Units.Pow(-1));
+
+        /// <inheritdoc/>
         public static T operator /(ValueWithUnits<T> left, Factor<T> right)
         {
             var units = left.Units / right;
@@ -67,8 +121,30 @@ namespace SiUnits
             }
         }
 
+        /// <inheritdoc/>
+        public static T operator checked /(ValueWithUnits<T> left, Factor<T> right)
+        {
+            checked
+            {
+                var units = left.Units / right;
+                try
+                {
+                    return left.Value * T.CreateChecked(units.AsConstant());
+                }
+                catch (InvalidOperationException ex)
+                {
+                    throw new InvalidOperationException($"Could not convert units of '{units}' to a constant.", ex);
+                }
+            }
+        }
+
+        /// <inheritdoc/>
         public static ValueWithUnits<T> operator +(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
             new ValueWithUnits<T>(left.Value + right / left.Units, left.Units);
+
+        /// <inheritdoc/>
+        public static ValueWithUnits<T> operator checked +(ValueWithUnits<T> left, ValueWithUnits<T> right) =>
+            new ValueWithUnits<T>(checked(left.Value + right / left.Units), left.Units);
 
         public static bool operator ==(ValueWithUnits<T> left, ValueWithUnits<T> right) => left.Equals(right);
 
